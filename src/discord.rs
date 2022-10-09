e8744e7f500e01c2fb0d9b7d1b0100973591af86
gapuchi <arjun.adhia@gmail.com>
2022-10-09 08:48:02 -0700

    Moved Low Level Discord Rest Calls

--- src/discord.rs
+++ src/discord.rs
@@ -1,7 +1,14 @@
+use std::collections::HashMap;
+use reqwest::header::{AUTHORIZATION, HeaderValue, InvalidHeaderValue};
+use reqwest::{Client, Error, Response};
 use serde::{Serialize, Deserialize};
+
+const BASE_URL: &'static str = "https://discord.com/api/v10";
 
 #[derive(Serialize, Deserialize)]
 pub struct Message {
+    pub id: String,
+    pub channel_id: String,
     pub content: String,
 }
 
@@ -12,4 +19,126 @@ pub struct User {
     pub avatar: String,
     pub email: Option<String>,
     pub flags: Option<String>,
+}
+
+pub struct HttpClient {
+    client: Client,
+    authorization_header: HeaderValue,
+}
+
+impl HttpClient {
+    pub fn new(bot_token: &str) -> Result<HttpClient, InvalidHeaderValue> {
+        Ok(HttpClient {
+            client: Client::new(),
+            authorization_header: HeaderValue::from_str(&format!("Bot {}", bot_token))?,
+        })
+    }
+
+    pub async fn get_user(&self, user_id: &str) -> Result<User, Error> {
+        self.client.get(&format!("{}/users/{}", BASE_URL, user_id))
+            .header(AUTHORIZATION, &self.authorization_header)
+            .send()
+            .await?
+            .json::<User>()
+            .await
+    }
+
+    pub async fn get_messages(&self, channel_id: &str) -> Result<Vec<Message>, Error> {
+        self.client.get(&format!("{}/channels/{}/messages", BASE_URL, channel_id))
+            .header(AUTHORIZATION, &self.authorization_header)
+            .send()
+            .await?
+            .json::<Vec<Message>>()
+            .await
+    }
+
+    pub async fn get_message(&self, channel_id: &str, message_id: &str) -> Result<Message, Error> {
+        self.client.get(&format!("{}/channels/{}/messages/{}", BASE_URL, channel_id, message_id))
+            .header(AUTHORIZATION, &self.authorization_header)
+            .send()
+            .await?
+            .json::<Message>()
+            .await
+    }
+
+    pub async fn create_message(&self, channel_id: &str, message: &str) -> Result<Message, Error> {
+        let content: String = message.parse().unwrap();
+
+        let mut map = HashMap::new();
+        map.insert("content", content);
+
+        self.client.post(&format!("{}/channels/{}/messages", BASE_URL, channel_id))
+            .header(AUTHORIZATION, &self.authorization_header)
+            .json(&map)
+            .send()
+            .await?
+            .json::<Message>()
+            .await
+    }
+
+    pub async fn edit_message(&self, channel_id: &str, message_id: &str, message: &str) -> Result<Message, Error> {
+        let content: String = message.parse().unwrap();
+
+        let mut map = HashMap::new();
+        map.insert("content", content);
+
+        self.client.patch(&format!("{}/channels/{}/messages/{}", BASE_URL, channel_id, message_id))
+            .header(AUTHORIZATION, &self.authorization_header)
+            .json(&map)
+            .send()
+            .await?
+            .json::<Message>()
+            .await
+    }
+
+    pub async fn delete_message(&self, channel_id: &str, message_id: &str) -> Result<Response,
+        Error> {
+        self.client.delete(&format!("{}/channels/{}/messages/{}", BASE_URL, channel_id, message_id))
+            .header(AUTHORIZATION, &self.authorization_header)
+            .send()
+            .await
+    }
+
+    pub async fn get_reactions(&self, channel_id: &str, message_id: &str, emoji: &str) ->
+                                                                                       Result<Vec<User>, Error> {
+        self.client.get(&format!("{}/channels/{}/messages/{}/reactions/{}", BASE_URL, channel_id, message_id, emoji))
+            .header(AUTHORIZATION, &self.authorization_header)
+            .send()
+            .await?
+            .json::<Vec<User>>()
+            .await
+    }
+
+    pub async fn create_reaction(&self, channel_id: &str, message_id: &str, emoji: &str) ->
+                                                                                         Result<Response, Error> {
+        self.client.put(&format!("{}/channels/{}/messages/{}/reactions/{}/@me", BASE_URL, channel_id, message_id, emoji))
+            .header(AUTHORIZATION, &self.authorization_header)
+            .send()
+            .await
+    }
+
+    pub async fn delete_reaction(&self, channel_id: &str, message_id: &str, emoji: &str) ->
+                                                                                         Result<Response, Error> {
+        self.client.delete(&format!("{}/channels/{}/messages/{}/reactions/{}/@me", BASE_URL, channel_id, message_id, emoji))
+            .header(AUTHORIZATION, &self.authorization_header)
+            .send()
+            .await
+    }
+
+    pub async fn delete_user_reaction(&self, channel_id: &str, message_id: &str, emoji: &str,
+                                      user_id: &str) -> Result<Response, Error> {
+        self.client.delete(&format!("{}/channels/{}/messages/{}/reactions/{}/{}", BASE_URL, channel_id, message_id, emoji, user_id))
+            .header(AUTHORIZATION, &self.authorization_header)
+            .send()
+            .await
+    }
+
+    pub async fn get_gateway(&self) -> Result<String, Error> {
+        self.client.get(&format!("{}/gateway", BASE_URL))
+            .header(AUTHORIZATION, &self.authorization_header)
+            .send()
+            .await?
+            .json()
+            .await
+    }
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,42 @@
-pub mod client;
 pub mod discord;
+
+pub mod high {
+    use crate::discord::HttpClient;
+    use reqwest::{Client, Error, Response};
+
+    pub struct Channel<'a> {
+        client: &'a HttpClient,
+        id: String,
+    }
+
+    pub struct Message<'a> {
+        client: &'a HttpClient,
+        id: String,
+        channel_id: String,
+        content: String,
+    }
+
+    impl Channel<'_> {
+        async fn send(&self, msg: &str) -> Result<Message, Error> {
+            let super::discord::Message { id, channel_id, content } = self.client
+                .create_message(&self.id, msg)
+                .await?;
+
+            Ok(Message {
+                client: self.client,
+                id,
+                channel_id,
+                content,
+            })
+        }
+    }
+
+    impl Message<'_> {
+        fn delete(&self) -> Result<(), Error> {
+
+            &self.client.delete_message(&*self.channel_id, &*self.id);
+
+            Ok(())
+        }
+    }
+}
